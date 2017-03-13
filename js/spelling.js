1528d7d1d67dfe2319e95184d4c8c34d7cf881d3
Jairo Roberto <jairo_roberto_8@hotmail.com>
2017-03-13 08:20:57 -0700

    Deletreador

--- js/spelling.js
+++ js/spelling.js
@@ -1,37 +1,11 @@
-function spellWord() {
+function spellWord(miCallback) {
     var word_to_spell = document.getElementById('word_to_spell').value.trim().split("");
 
     for (var i = 0; i < word_to_spell.length; i++) {
     	document.getElementById("audio").src = 'audios/alphabet/' + word_to_spell[i].toUpperCase() + word_to_spell[i].toLowerCase() + '.mp3';
-        var sound = document.getElementById("verb_audio");
+        var sound = document.getElementById("audio");    
 
-        if (i > 0){
-        	try {
-	            sound.load();
-	            sound.play();
-       		} catch (e) {
-            	alert('Ocurrio un error al reproducir el audio');
-        	}
-        }
-        
-        
-        (function(){
-
-          	setTimeout(myTimer, 2000);
-
-     	})();
-
+        sound.play();
     }
 
-}
-
-function myTimer() {
-    var sound = document.getElementById("verb_audio");
-
-    try {
-            sound.load();
-            sound.play();
-        } catch (e) {
-            alert('Ocurrio un error al reproducir el audio');
-        }
 }
