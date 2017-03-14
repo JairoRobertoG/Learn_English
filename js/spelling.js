ebade54e076e9444c15432785a898ba9dcfc4303
Luis Ivan Calva Lopez <Luis Ivan Calva Lopez>
2017-03-14 01:57:01 -0700

    Spell

--- js/spelling.js
+++ js/spelling.js
@@ -1,11 +1,27 @@
-function spellWord(miCallback) {
-    var word_to_spell = document.getElementById('word_to_spell').value.trim().split("");
+function reproducir() {
+    var palabra = document.getElementById("word_to_spell");
+    if(palabra.value.length ==0){
+        alert("Nada que deletrear");
+        return;
+    }
+    word_to_spell = document.getElementById('word_to_spell').value.trim().split("");
 
-    for (var i = 0; i < word_to_spell.length; i++) {
-    	document.getElementById("audio").src = 'audios/alphabet/' + word_to_spell[i].toUpperCase() + word_to_spell[i].toLowerCase() + '.mp3';
+    function spellConTime(i) {
+        if (i >= palabra.value.length) {
+            return;
+        }
+        var dummy = setTimeout(function () {
+            playSound('audios/alphabet/' + word_to_spell[i].toUpperCase() + word_to_spell[i].toLowerCase() + '.mp3');
+            i++;
+            spellConTime(i);
+        }, 2500);
+    }
+    spellConTime(0);
+}
+
+function playSound(src) {
+    document.getElementById("audio").src = src;
         var sound = document.getElementById("audio");    
 
         sound.play();
-    }
-
 }
